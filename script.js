3de12a308cc091d3e11bbbf17a023f5c7fe86e6c
karimlrb <japan-rimk@hotmail.fr>
2021-10-29 03:03:26 +0200

    version avec l'affichage de la question et des choix sans interaction au clic

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 class Question {
   constructor(text, choices, answer) {
     this.text = text;
-    this.choices;
+    this.choices = choices;
     this.answer = answer;
   }
   //Si le choix du user est = à this.answer on renvoie true, c'est un booléen
@@ -10,7 +10,7 @@ class Question {
   }
 }
 
-let question = [
+let questions = [
   new Question(
     "Quelle méthode Javascript permet de filtrer les éléments d'un tableau",
     ["indexOf()", "map()", "filter()", "reduce()"],
@@ -18,7 +18,8 @@ let question = [
   ),
   new Question(
     "Quelle méthode Javascript permet de vérifier si un élément figure dans un tableau",
-    ["isNaN()", "includes()", "findIndex()", "isOdd()", "includes()"]
+    ["isNaN()", "includes()", "findIndex()", "isOdd()", "includes()"],
+    "includes()"
   ),
   new Question(
     "Quelle méthode transforme du JSON en un objet Javascript",
@@ -39,7 +40,7 @@ class Quiz {
     this.currentQuestionIndex = 0;
   }
   getCurrentQuestion() {
-    this.questions[this.currentQuestionIndex];
+    return this.questions[this.currentQuestionIndex];
   }
   guess(answer) {
     if (this.getCurrentQuestion().isCorrectAnswer(answer)) {
@@ -52,4 +53,54 @@ class Quiz {
   }
 }
 
+// regroup all functions relative to the app display
+
+const display = {
+  // ici déclarer la fonction -> fonction elementShown(id, text) {} ne marche pas, utiliser la syntaxe ci-dessous
+
+  elementShown: function (id, text) {
+    let element = document.getElementById(id);
+    element.innerHTML = text;
+  },
+  endQuiz: function () {
+    endQuizHTML = `
+        <h1>Quiz terminé !</h1>
+        <h3> Votre score est de : ${quiz.score} / ${quiz.questions.length}</h3>`;
+    this.elementShown("quiz", endQuizHTML);
+  },
+  question: function () {
+    this.elementShown("question", quiz.getCurrentQuestion().text);
+  },
+  choices: function () {
+    let choices = quiz.getCurrentQuestion().choices;
+
+    guessHandler = (id, guess) => {
+      document.getElementById(id).onclik = function () {
+        quiz.guess(guess);
+        quizApp();
+      };
+    };
+    for (let i = 0; i < choices.length; i++) {
+      this.elementShown("choice" + i, choices[i]);
+    }
+  },
+};
+// game logic
+
+quizApp = () => {
+  // on test si quiz.hasEnded() === true
+  if (quiz.hasEnded()) {
+    display.endQuiz();
+  } else {
+    display.question();
+    display.choices();
+    // progress
+  }
+};
+
 // create quiz
+
+let quiz = new Quiz(questions);
+quizApp();
+
+console.log(quiz);
